8087b9a7324ee7e44e66bfa7260adb793470ce5b
Rafael.Silva <rafaeldeoliveira88@bol.com.br>
2020-06-22 16:56:39 -0300

    Ajuste de URL

--- web_server/public/js/appFrontEnd.js
+++ web_server/public/js/appFrontEnd.js
@@ -27,7 +27,7 @@ cotacoesForm.addEventListener('submit', (event) => {
         return;
     }
 
-    fetch(`http://localhost:3000/cotacoes?ativo=${ativo}`).then((response) => {
+    fetch(`/cotacoes?ativo=${ativo}`).then((response) => {
         response.json().then((data) => {
             if (data.error){
                 mainMensage.innerHTML = `Alguma coisa deu errado: ${data.error.message} | código: ${data.error.code}`
--- web_server/src/app.js
+++ web_server/src/app.js
@@ -85,8 +85,11 @@ app.get('*', (req, res) => {
 //-----------------------------------------------------------------------------------//
 
 //Informa que a aplicação estará escutando na porta 3000
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
 })
 
 //-----------------------------------------------------------------------------------//
